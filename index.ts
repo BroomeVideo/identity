1cd532461ddd818abdcedb790593d59dba9069e0
Christian Nunciato <c@nunciato.org>
2019-01-16 16:56:02 -0800

    Refactor a bit, add some light documentation
    Signed-off-by: Christian Nunciato <c@nunciato.org>

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import { AWSImportExportReadOnlyAccess } from "@pulumi/aws/iam";
 
-// Create a bot user for CI/CD.
+// Create a bot user for managing Kubernetes infrastucture and applications in CI/CD scenarios.
 const botUser = new aws.iam.User("botUser");
 
 // Create an access key for the bot user.
@@ -13,6 +12,19 @@ const botKey = new aws.iam.AccessKey("botKey", {
 type Policies = {
     [name: string]: pulumi.Input<aws.ARN>
 };
+
+const assumeRolePolicy = botUser.arn.apply(arn => (<aws.iam.PolicyDocument>{
+    Version: "2012-10-17",
+    Statement: [
+        {
+            Effect: "Allow",
+            Principal: {
+                AWS: arn,
+            },
+            Action: "sts:AssumeRole",
+        }
+    ],
+}));
 
 function roleWithPolicies(name: string, args: aws.iam.RoleArgs, policies: Policies): aws.iam.Role {
     const role = new aws.iam.Role(name, args);
@@ -32,49 +44,30 @@ function roleWithPolicies(name: string, args: aws.iam.RoleArgs, policies: Polici
     return role;
 }
 
-const assumeRolePolicy = botUser.arn.apply(arn => (<aws.iam.PolicyDocument>{
-    Version: "2012-10-17",
-    Statement: [
-        {
-            Effect: "Allow",
-            Principal: {
-                AWS: arn,
-            },
-            Action: "sts:AssumeRole",
-        }
-    ],
-}));
-
-const infraPolicies = {
-    "adminAccess": aws.iam.AdministratorAccess,
-};
-
-const appPolicies = {
-    "ecrPowerUser": aws.iam.AmazonEC2ContainerRegistryPowerUser,
-};
-
-export const infraRole = roleWithPolicies(
+// The infrastructure role allows the bot user to create and manage infrastructure.
+// https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_job-functions.html#jf_administrator
+const infraRole = roleWithPolicies(
     "infraRole",
     {
-        description: "Infrastructure role for CI users",
         assumeRolePolicy: assumeRolePolicy,
     },
-    infraPolicies
+    {
+        adminAccess: aws.iam.AdministratorAccess,
+    }
 );
 
-export const appRole = roleWithPolicies(
+// The app role allows the bot user to interact with the Amazon ECR service.
+// https://docs.aws.amazon.com/AmazonECR/latest/userguide/ecr_managed_policies.html#AmazonEC2ContainerRegistryPowerUser
+const appRole = roleWithPolicies(
     "appRole",
     {
-        description: "Application role for CI users",
         assumeRolePolicy: assumeRolePolicy,
     },
-    appPolicies
+    {
+        ecrPowerUser: aws.iam.AmazonEC2ContainerRegistryPowerUser,
+    }
 );
 
-// Export the bot for use with other stacks.
-// export const botUserArn = botUser.arn
-// export const botUserName = botUser.name
-// export const botUserKeyId = botKey.id
-// export const botUserKeySecret = botKey.secret
+// Export the infrastructure and application roles for use by the infrastructure stack.
 export const infraRoleArn = infraRole.arn;
 export const appRoleArn = appRole.arn;
